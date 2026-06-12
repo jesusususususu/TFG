b720a67f4c59b23f5bf6e88a0c3a294b6422b724
jesusususususu <jesusbenitogonzalez1234@gmail.com>
2026-06-12 18:30:17 +0200

    fix: mejoramos las clases AppDatabase y DetalleRecetaActivity y las actualizamos

--- app/src/main/java/com/example/tfg/DetalleRecetaActivity.java
+++ app/src/main/java/com/example/tfg/DetalleRecetaActivity.java
@@ -1,23 +1,25 @@
+
 package com.example.tfg;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetalleRecetaActivity extends AppCompatActivity {
 
-    TextView nombre, ingredientes, pasos, tiempo;
-    ImageView imagen;
+    private TextView nombre, ingredientes, pasos, tiempo;
+    private ImageView imagen;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_receta);
 
         nombre = findViewById(R.id.txtNombreDetalle);
-        ingredientes = findViewById(R.id.txtIngredientes);
+        ingredientes = findViewById(R.id.txtListaIngredientes);
         pasos = findViewById(R.id.txtPasos);
         tiempo = findViewById(R.id.txtTiempoDetalle);
         imagen = findViewById(R.id.imgDetalle);
@@ -31,5 +33,8 @@ public class DetalleRecetaActivity extends AppCompatActivity {
             tiempo.setText(receta.getTiempoPreparacion() + " min");
             imagen.setImageResource(receta.getImagenResurce());
         }
+
+
+        findViewById(R.id.btnVolverRecetas).setOnClickListener(v -> finish());
     }
 }
